c40dddd22c33ae6370d69d8ec3d4d347dd1b9c77
Florian Frank <flori@ping.de>
2012-11-28 16:36:03 +0100

    more modules

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ ln -sf `pwd`/.vim $HOME/.vim
 for plugin in vim-plugins/*/
 do
   p=`basename $plugin`
-  stow --verbose --dir=vim-plugins --target=.vim --override='(README|readme)' "$p"
+  stow --verbose --dir=vim-plugins --target=.vim --override='.*' "$p"
 done
